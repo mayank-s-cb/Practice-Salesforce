4b64671fbf682ba2158831d93aef991aa049d30c
Mayank Shrivastava <159246735+mayank-s-cb@users.noreply.github.com>
2024-05-02 17:31:46 +0530

    Create secondJs.js

---
+++ scripts/js/secondJs.js
@@ -0,0 +1,29 @@
+const process = require('process');
+
+const fs = require('fs').promises; // Use fs.promises for async file operations
+
+async function readXmlFile(filePath) {
+    try {
+        // Read the XML file asynchronously
+        profileXmlFile = await fs.readFile(filePath, 'utf8');
+        return profileXmlFile;
+    } catch (error) {
+        console.error('Error reading XML file:', error);
+        throw error;
+    }
+}
+
+async function main(){
+    readXmlFile(filePath)
+    .then( data => {
+        var newData = data.toString()+firstJsValue;
+        fs.writeFile(filePath, newData);
+    })
+}
+
+  
+  // Retrieve the string value passed from the GitHub Action
+  //const stringValue = process.env.newData;
+  const stringValue = 'New Data';
+  
+  main(stringValue, './newDay.txt')
